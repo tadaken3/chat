8e16c6b941c02a9eab171aac9d0cc72b77bdbf8b
tadaken3 <k.tanaka6057@gmail.com>
2017-08-23 20:38:48 +0900

    init repo

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates",
+				t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+	gomniauth.SetSecurityKey("doraemon315")
+	gomniauth.WithProviders(
+		google.New("424841365323-hfmtug0okb7drhutpku2ie0sjv4rv3u0.apps.googleusercontent.com", "d2faci1amuBYUf8L1wvlQ_ZD", "http://localhost:8080/auth/callback/google"),
+	)
+	r := newRoom()
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/room", r)
+	go r.run()
+	log.Println("Webサーバーを開始します。ポート:", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
