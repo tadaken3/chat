52d9dad6a3d76aa9178b9de78000e25b61a530c4
tadaken3 <k.tanaka6057@gmail.com>
2017-09-01 23:51:30 +0900

    add message.go

--- room.go
+++ room.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/gorilla/websocket"
+	"github.com/stretchr/objx"
 	"github.com/tadaken3/trace"
 	"log"
 	"net/http"
 )
 
 type room struct {
-	forward chan []byte
+	forward chan *message
 	join    chan *client
 	leave   chan *client
 	clients map[*client]bool
@@ -17,7 +18,7 @@ type room struct {
 
 func newRoom() *room {
 	return &room{
-		forward: make(chan []byte),
+		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
@@ -36,7 +37,7 @@ func (r *room) run() {
 			r.tracer.Trace("leave user")
 			close(client.send)
 		case msg := <-r.forward:
-			r.tracer.Trace("recived message:", string(msg))
+			r.tracer.Trace("recived message:", string(msg.Message))
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
@@ -64,10 +65,16 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		log.Fatal("ServeHTTP:", err)
 		return
 	}
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatal("クッキーの取得に失敗しました:", err)
+		return
+	}
 	client := &client{
-		socket: socket,
-		send:   make(chan []byte, messageBufferSize),
-		room:   r,
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
 	}
 	r.join <- client
 	defer func() { r.leave <- client }()
